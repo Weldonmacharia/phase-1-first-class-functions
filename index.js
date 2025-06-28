75ac787db0a51943c01a9e739d11c07dd31fb5c5
Weldon Macharia <weldonmacharia28@gmail.com>
2025-06-29 02:24:18 +0300

    assingnm

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function receivesAFunction(callback) {
+  callback();
+}
+
+
+function returnsANamedFunction() {
+  function namedFunction() {
+    return "I am a named function";
+  }
+  return namedFunction;
+}
+
+
+function returnsAnAnonymousFunction() {
+  return function() {
+    return "I am anonymous";
+  };
+}
